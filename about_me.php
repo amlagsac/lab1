afdf67565c54fea6a81b2b41399967b1651d40c0
amlagsac <51469640+amlagsac@users.noreply.github.com>
2019-07-15 08:02:10 +0800

    Update about_me.php

--- about_me.php
+++ about_me.php
@@ -14,10 +14,10 @@
         <div class = "header">
             <div class = "inner_header">
                 <div class = "web_title">
-                    <a href = "index.html"><h1>C.S.<span>Diaries</span></h1></a>
+                    <a href = "index.php"><h1>C.S.<span>Diaries</span></h1></a>
                 </div>
                 <ul class = "navigation">
-                    <a href = "index.html"><li>Home</li></a>
+                    <a href = "index.php"><li>Home</li></a>
                     <a><li>Activities
                         <ul class = "inner_navigation">
                         <li>
@@ -31,7 +31,7 @@
                         </li>
                         </ul>
                     </li></a>
-                    <a href="about_me.html"><li>About Me</li></a>
+                    <a href="about_me.php"><li>About Me</li></a>
                 </ul>
             </div>
         </div>
